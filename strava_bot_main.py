b7053e40d93b7acd4736eebb0c4b34d520d58c5c
adamantium7 <lucky.ayuste@gmail.com>
2023-05-31 11:59:32 -0700

    change from client to bot

--- strava_bot_main.py
+++ strava_bot_main.py
@@ -1,22 +1,29 @@
 import discord
+from discord.ext import commands
 
 TOKEN = 'MTExMzUwMjg4Njk0NTYyMDA4MA.GvyyGP.625SpJYyUXlsSTNhykK_LSq9V4wVQNLD0QU-RA'
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = discord.Bot(command_prefix = '$',intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'we have logged in as {client.user}')
+    print(f'we have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(TOKEN)
+bot.run(TOKEN)
+
+#shutdown bot
+@bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    exit()
