8fbd5441033a1c44b354ea4168f0bc6440f8866a
adamantium7 <lucky.ayuste@gmail.com>
2023-06-06 09:47:23 -0700

    added user auth

--- strava.py
+++ strava.py
@@ -4,6 +4,19 @@ import os
 from dotenv import load_dotenv
 from stravalib import Client
 
+#get client id and secret from .env
 load_dotenv()
 CLIENT_SECRET = os.getenv('STRAVA_CLIENT_SECRET')
 CLIENT_ID = os.getenv('STRAVA_CLIENT_ID')
+
+client = Client()
+
+#Get URL for user to authenticate
+#TODO: find way to get code (for token) after user authenticates
+url = client.authorization_url(client_id=CLIENT_ID, 
+                               redirect_uri='http://127.0.0.1:5000/authorization',
+                                scope=['read_all','profile:read_all','activity:read_all'])
+
+#print(url)
+CODE = ''
+#access_token = client.exchange_code_for_token(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=CODE)
