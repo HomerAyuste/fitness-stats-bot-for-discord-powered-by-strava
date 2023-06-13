d618c8baf0050637581ca96c76973ecbcb293935
adamantium7 <lucky.ayuste@gmail.com>
2023-06-13 16:12:55 -0700

    add grid to recap graph and change distance from m to km

--- graphs_and_stats.py
+++ graphs_and_stats.py
@@ -9,10 +9,14 @@ def recap(df, title, column='month_and_year', y_column='moving_time_hr'):
         df[column] = df['month_of_year'].map(str) + '-' + df['year'].map(str)
         df[column] = pd.to_datetime(df[column],format='%m-%Y')
         df[column] = df[column].dt.strftime('%y-%m')
+    if y_column =='distance':
+        df.distance = df.distance/1000
     df1 = df.groupby(df[column].values)[y_column].sum()
-    df1.plot(kind='bar')
+    ax = df1.plot(kind='bar')
+    ax.set_axisbelow(True)
     plt.title(title)
-    plt.ylabel('Hours' if y_column=='moving_time_hr' else 'Distance (m)')
+    plt.ylabel('Hours' if y_column=='moving_time_hr' else 'Distance (km)')
+    plt.grid(axis='y')
     #save image in data stream
     data_stream = io.BytesIO()
     #plt.figure(figsize=(10,6))
