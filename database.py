3e3110fbbb341580f77eb9a2c743cfb6aefc99c1
adamantium7 <lucky.ayuste@gmail.com>
2023-06-19 08:57:25 -0700

    added table creation and function for inserting to table

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 import sqlite3
 
+TABLE = "user_codes"
 con = sqlite3.connect("strava_bot.db")
 cur = con.cursor()
 
-if(cur.execute("SELECT name FROM sqlite_master WHERE name=''") is None):
-    cur.execute("CREATE TABLE ")
+if(cur.execute(f"SELECT name FROM sqlite_master WHERE name='{TABLE}'") is None):
+    cur.execute(f"""CREATE TABLE {TABLE}
+                (user_id INTEGER NOT NULL PRIMARY KEY,
+                auth_code ,
+                access_token)""")
+
+def insert_val(user_id,auth_code,access_token):
+    cur.execute(f"INSERT INTO {TABLE} VALUES(?,?,?)",(user_id,auth_code,access_token))
