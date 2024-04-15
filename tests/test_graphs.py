9ee768d88e1a3418a611e0ee910c194ef2225756
HomerAyuste <lucky.ayuste@gmail.com>
2024-04-15 00:57:03 -0400

    added dataframe fixture for testing

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -1,5 +1,39 @@
 import pytest
 from bot.graphs_and_stats import *
+stat = stats()
 
-def test_cumulative_graph():
-    assert isinstance(cumululative_graph(), File)
+@pytest.fixture
+def example_df():
+    my_cols =['name',
+        'start_date_local',
+        'type',
+        'distance',
+        'moving_time',
+        'elapsed_time',
+        'total_elevation_gain',
+        'elev_high',
+        'elev_low',
+        'average_speed',
+        'max_speed',
+        'average_heartrate',
+        'max_heartrate',
+        'start_latitude',
+        'start_longitude',
+        'distance_km',
+        'start_date_local',
+        'day_of_week',
+        'month_of_year',
+        'year',
+        'moving_time',
+        'elapsed_time',
+        'elapsed_time_hr',
+        'moving_time_hr',
+        'average_speed_kph',
+        'max_speed_kph']
+    
+    df = pd.DataFrame(columns=my_cols)
+    #df.loc[0] = ['','','','']
+    return df
+
+def test_cumulative_graph(example_df):
+    assert isinstance(stat.cumulative_graph(example_df,"","moving_time_hr",""), File)
