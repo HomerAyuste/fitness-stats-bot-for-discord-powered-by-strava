813a3bd82705d59d4444b549066f829367e716b4
HomerAyuste <lucky.ayuste@gmail.com>
2024-06-04 14:17:23 -0700

    updated install.sh to add some missing steps including cd into repo and creating env file

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #clone repo
 git clone https://github.com/HomerAyuste/fitness-stats-bot-for-discord-powered-by-strava.git
-##TODO: cd into repo
+##change directory to repo
+cd fitness-stats-bot-for-discord-powered-by-strava
 #create virtual environment
-py -m venv /run_FS_bot/
-run_FS_bot/Scripts/activate
-
+py -m venv /env/
+env/Scripts/activate ##works only for windows users
 #install packages
 pip install -r requirements.txt
+#create .env file for secrets
+echo "DISCORD_TOKEN = ...
+DISCORD_CLIENT_SECRET = ...
+STRAVA_CLIENT_SECRET = ...
+STRAVA_CLIENT_ID = ..." >> .env
+
+#setup done
+echo "The bot is almost set! Please fill in the necessary secrets in the .env file then you can run the bot."
