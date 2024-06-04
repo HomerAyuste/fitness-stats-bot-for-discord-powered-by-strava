43a74a97c0d0efdeeb530499710c2d14213e08c9
HomerAyuste <lucky.ayuste@gmail.com>
2024-06-04 13:59:16 -0700

    create install.sh to simplify setting up bot

---
+++ install.sh
@@ -0,0 +1,9 @@
+#clone repo
+git clone https://github.com/HomerAyuste/fitness-stats-bot-for-discord-powered-by-strava.git
+##TODO: cd into repo
+#create virtual environment
+py -m venv /run_FS_bot/
+run_FS_bot/Scripts/activate
+
+#install packages
+pip install -r requirements.txt
